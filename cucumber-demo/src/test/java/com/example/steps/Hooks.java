95d01df0a96e4088e79df1610c344ab45d293028
aepifanova393-lgtm <a.epifanova393@gmail.com>
2026-08-16 16:57:58 +0300

    Initial commit

---
+++ cucumber-demo/src/test/java/com/example/steps/Hooks.java
@@ -0,0 +1,10 @@
+package com.example.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+
+
+}
---
+++ cucumber-demo/src/test/java/com/example/steps/Steps.java
@@ -0,0 +1,18 @@
+package com.example.steps;
+
+import com.example.calculator.Calculator;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.То;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Steps {
+
+
+}
